a6c101cdaa768f767ef807da945daf185f4a85d7
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-24 17:39:05 +0300

    coverpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './App.css';
+
 
 // Import the Cover component
 import Cover from '/home/leila/development/code/phase-3/aa-frontend/src/cover.js';
--- src/cover.js
+++ src/cover.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
+import './App.css';
 const Cover = () => {
   return (
-    <div>
+    <div className="Cover-image"> {/* Apply the Cover-image class here */}
       <h1>Welcome to A.A Family Hospitals</h1>
-      <img
-        src={process.env.PUBLIC_URL + 'https://i.pinimg.com/564x/e4/14/02/e414020ea436f0a58eac0f3ffa6d5ab8.jpg'}
-        alt="Hospital"
-        style={{ width: '100%', maxWidth: '600px', marginTop: '20px' }}
-      />
+      {/* The rest of your content */}
+     
+     
     </div>
   );
 };
